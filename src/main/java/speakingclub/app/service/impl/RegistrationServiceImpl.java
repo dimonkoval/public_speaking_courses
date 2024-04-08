e1ebd1d2fa7a98fc84d42aebea78d587784b2926
Maks Hrytsyna <hrytsynamax@gmail.com>
2024-04-09 00:09:13 +0300

    User role auto saving

--- src/main/java/speakingclub/app/service/impl/RegistrationServiceImpl.java
+++ src/main/java/speakingclub/app/service/impl/RegistrationServiceImpl.java
@@ -7,7 +7,10 @@ import speakingclub.app.dto.user.UserRegistrationRequestDto;
 import speakingclub.app.dto.user.UserResponseDto;
 import speakingclub.app.exception.RegistrationException;
 import speakingclub.app.mapper.user.UserMapper;
+import speakingclub.app.model.Role;
 import speakingclub.app.model.User;
+import speakingclub.app.model.enums.RoleName;
+import speakingclub.app.repository.user.RoleRepository;
 import speakingclub.app.repository.user.UserRepository;
 import speakingclub.app.service.RegistrationService;
 
@@ -17,6 +20,7 @@ public class RegistrationServiceImpl implements RegistrationService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto registerUser(UserRegistrationRequestDto requestDto) {
@@ -24,8 +28,10 @@ public class RegistrationServiceImpl implements RegistrationService {
             throw new RegistrationException("Can't register user with email "
                     + requestDto.getEmail());
         }
+        Role userRole = roleRepository.findRoleByRoleName(RoleName.USER);
         User user = userMapper.toModel(requestDto);
         user.setPassword(passwordEncoder.encode(requestDto.getPassword()));
+        user.getRoles().add(userRole);
         User savedUser = userRepository.save(user);
         return userMapper.toDto(savedUser);
     }
